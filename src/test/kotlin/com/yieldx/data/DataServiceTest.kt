209d8354ddf21a12431215c864ddb9c55ae2bb20
Laukii Cheng <laukii.cheng@gmail.com>
2021-08-11 10:53:24 -0500

    move unit test into kotlin folder

---
+++ src/test/kotlin/com/yieldx/data/DataServiceTest.kt
@@ -0,0 +1,35 @@
+package com.yieldx.data
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import io.kotest.matchers.shouldBe
+import io.kotest.provided.BaseStringSpec
+import io.mockk.every
+import io.mockk.mockk
+import org.springframework.core.io.Resource
+import org.springframework.core.io.ResourceLoader
+
+internal class DataServiceTest : BaseStringSpec() {
+    init {
+        val fileContents =
+            """
+            name,number
+            laukii,3
+            kahili,7
+            """.trimIndent()
+
+        val resource = mockk<Resource> {
+            every { inputStream } returns fileContents.byteInputStream()
+        }
+        val resourceLoader = mockk<ResourceLoader> {
+            every { getResource(any()) } returns resource
+        }
+        val objectMapper = mockk<ObjectMapper>()
+        val service = DataService(objectMapper, resourceLoader)
+
+        "getDataFromCsvFile should return list of ${Finance::class.simpleName}" {
+            val financeList = service.getDataFromCsvFile("")
+
+            financeList.size shouldBe 2
+        }
+    }
+}
